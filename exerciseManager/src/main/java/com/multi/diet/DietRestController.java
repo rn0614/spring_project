b2ab8348070b8ae7fe297feca452fb2d6c62c4a1
Koo SangMo <rn0614@naver.com>
2021-06-17 09:29:35 +0900

    운동달력 에러 수정

--- exerciseManager/src/main/java/com/multi/diet/DietRestController.java
+++ exerciseManager/src/main/java/com/multi/diet/DietRestController.java
@@ -35,14 +35,14 @@ public class DietRestController {
 	
 	
 	@RequestMapping("/dietOCR")
-	public NutriFactVO dietOCR(MultipartHttpServletRequest request, @RequestParam("uploadFile") MultipartFile file) {
+	public NutriFactVO dietOCR(MultipartHttpServletRequest request1, @RequestParam("uploadFile") MultipartFile file) {
 		/* String result = ""; */
 		NutriFactVO nfvo = new NutriFactVO();		
 		
 		  try {
-			  String uploadPath = request.getSession().getServletContext().getRealPath("/fileresources/");
+			  String uploadPath = request1.getSession().getServletContext().getRealPath("/fileresources/");
 					  /*"c:/ai/";*/
-			  
+			  System.out.println(uploadPath);
 			  String originalFileName = file.getOriginalFilename();  
 			  
 			  String filePathName = uploadPath + originalFileName;
@@ -166,6 +166,12 @@ public class DietRestController {
 		
 		return foodList;
 	}
+	/*
+	 * public String getUploadPath(MultipartHttpServletRequest request) { String
+	 * uploadPath =
+	 * request.getSession().getServletContext().getRealPath("/fileresources/");
+	 * return uploadPath; }
+	 */
 	
 	
 	
